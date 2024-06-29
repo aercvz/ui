6142fc30d0f79e99a4f8cadf1e5ccbda0df6f9ed
aercvz <103784616+aercvz@users.noreply.github.com>
2024-06-29 18:03:00 +0300

    Update fork.lua

--- fork.lua
+++ fork.lua
@@ -2464,7 +2464,7 @@ function library.new(library_title, cfg_location, color)
 						SliderButton.MouseButton1Down:Connect(function()
 							SliderFrame.Size = UDim2.new(0, math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 260), 1, 0)
 
-							local val = math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
+							local val = math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 1) * max --math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
 							if val ~= value.Slider then
 								SliderValue.Text = val
 								value.Slider = val
@@ -2476,7 +2476,7 @@ function library.new(library_title, cfg_location, color)
 							move_connection = mouse.Move:Connect(function()
 								SliderFrame.Size = UDim2.new(0, math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 260), 1, 0)
 
-								local val = math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
+								local val =math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 1) * max -- math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
 								if val ~= value.Slider then
 									SliderValue.Text = val
 									value.Slider = val
@@ -2487,7 +2487,7 @@ function library.new(library_title, cfg_location, color)
 								if Mouse.UserInputType == Enum.UserInputType.MouseButton1 then
 									SliderFrame.Size = UDim2.new(0, math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 260), 1, 0)
 
-									local val = math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
+									local val = math.clamp(mouse.X - SliderFrame.AbsolutePosition.X, 0, 1) * max -- math.floor((((max - min) / 260) * SliderFrame.AbsoluteSize.X) + min)
 									if val ~= value.Slider then
 										SliderValue.Text = val
 										value.Slider = val
